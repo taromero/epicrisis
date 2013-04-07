7aeabb9595b09b33659bd4f82252e0af22205e17
Tomas Romero <canotto90@gmail.com>
2013-04-07 15:57:28 -0300

    Funcionan los shotcuts cuando el focus esta en un input de texto

--- web-app/js/shortcuts.js
+++ web-app/js/shortcuts.js
@@ -1,13 +1,23 @@
 $(function(){	
 	$(document).bind('keydown', 'alt+ctrl+e', agregarDroga);
-	$(document).bind('keydown', 'alt+ctrl+w', function() {
-		drogasCounter--;
-		borrarDroga(drogasCounter);
-	});
-	$(document).bind('keydown', 'ctrl+return', function() {
-		$('#save').click();
-	});
-	$(document).bind('keydown', 'ctrl+backspace', function() {
-		$('#cancelar').click();
-	});
+	$(document).bind('keydown', 'alt+ctrl+w', bindeoBorrarDroga);
+	$(document).bind('keydown', 'ctrl+return', abrirModalSave);
+	$(document).bind('keydown', 'ctrl+backspace', abrirModalCancelar);
+	$(document).find(':input').bind('keydown', 'alt+ctrl+e', agregarDroga);
+	$(document).find(':input').bind('keydown', 'alt+ctrl+w', bindeoBorrarDroga);
+	$(document).find(':input').bind('keydown', 'ctrl+return', abrirModalSave);
+	$(document).find(':input').bind('keydown', 'ctrl+backspace', abrirModalCancelar);
 });
+
+function bindeoBorrarDroga() {
+	drogasCounter--;
+	borrarDroga(drogasCounter);
+}
+
+function abrirModalSave() {
+	$('#save').click();
+}
+
+function abrirModalCancelar() {
+	$('#cancelar').click();
+}
